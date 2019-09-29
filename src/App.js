4f387dc7426539fb2f6c326aac03d5cef2634a40
Will Ginsberg <will.j.ginsberg@gmail.com>
2019-09-29 16:27:05 -0400

    Spotify authorization and playlist endpoint

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { authorize, playlists } from './SpotifyApiService';
+
+class App extends React.Component {
+
+  componentDidMount() {
+    authorize();
+    playlists();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/SpotifyApiService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const client_id = '3680b1abca094e458e2a9f190f5607d1';
+const redirect_uri = 'http://localhost:3000';
+
+let authToken;
+
+const authorize = () => {
+  console.log('authorizing...');
+
+  const hashFragment = document.location.hash.split('&').find( s => s.match(/#?access_token=/))
+  const accessToken = hashFragment ? hashFragment.split('=')[1] : '';
+
+  if (accessToken) {
+    authToken = accessToken;
+    axios.defaults.headers.common['Authorization'] = `Bearer ${authToken}`;
+  } else {
+    var url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&scope=' + encodeURIComponent('playlist-read-private');
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+
+    window.location = url;
+  }
+}
+
+const playlists = () => {
+  console.log('fetching playlists...');
+
+  return axios.get('https://api.spotify.com/v1/me/playlists').then(response => console.log(response));
+}
+
+export { authorize, playlists };
