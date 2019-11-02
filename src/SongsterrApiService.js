432e8dc8740cd48cf59b09bd81948893fadabe46
Will Ginsberg <will.j.ginsberg@gmail.com>
2019-11-02 15:24:41 -0400

    Cleanup

--- src/SongsterrApiService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const tabsByArtist = artist => {
-  // const baseUrl = 'https://www.songsterr.com/a/ra/songs.json';
-  // return axios.get(`${baseUrl}?pattern=${encodeURIComponent(artist)}`)
-  return axios.get('https://www.songsterr.com/a/ra/songs/byartists.json?artists=metallica');
-};
-
-export { tabsByArtist };
--- src/util.js
+++
@@ -1,8 +0,0 @@
-const generateSongersterrURLs = 
-
-// Returns a promise resolving URLs for tabs given a list of tracks
-const generateTabURLs = spotifyTracks => {
-    
-}
-
-export { generateTabURLS };
