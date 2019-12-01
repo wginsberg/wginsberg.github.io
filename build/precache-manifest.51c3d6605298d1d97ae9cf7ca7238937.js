9e3deb0b8f06d0de266d76fe368efb2a9a22bea6
Will Ginsberg <will.j.ginsberg@gmail.com>
2019-12-01 15:40:43 -0500

    Add buttons for spotify and ultimate guitar

---
+++ build/precache-manifest.51c3d6605298d1d97ae9cf7ca7238937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d224d26d14b7714569d75fc6aafc83fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9adba0d30643fecb722c",
+    "url": "/static/css/main.0cc7b10a.chunk.css"
+  },
+  {
+    "revision": "eff3f73c9c0130fc3b54",
+    "url": "/static/js/2.72728c58.chunk.js"
+  },
+  {
+    "revision": "9adba0d30643fecb722c",
+    "url": "/static/js/main.154dd35b.chunk.js"
+  },
+  {
+    "revision": "2e832e8c74bfc35f6363",
+    "url": "/static/js/runtime-main.6ed9e85e.js"
+  },
+  {
+    "revision": "3fbf5ce53042c8726d187791386e2db3",
+    "url": "/static/media/songsterr.3fbf5ce5.svg"
+  }
+]);
